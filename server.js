2de79cd597f8ef7b14220b78410630191a641915
UrjaKhimania <30670182+UrjaKhimania@users.noreply.github.com>
2017-08-03 19:44:36 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,54 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var essay = {
+    title: "Technology Article",
+    heading: "What Really is the Impact of Technology on Teens?",
+    date: `August 3rd, 2017
+            --By Urja Khimania`,
+    content: "heeeeeeeeeeeeeeeeeeuuuuuuyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy",
+};
+
+function createTemplate (data){
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    var htmlTemplate = 
+       `<!doctype html>
+    <html>
+        <head>
+            <title>
+            ${title}
+            </title>
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body background="https://s-media-cache-ak0.pinimg.com/236x/e6/53/77/e65377a1908647e5404ea92ffb9b015b--solid-iphone-wallpaper
+            -iphone--plus-wallpapers.jpg">
+            <div class="container">
+                <p><h2>
+                ${heading}
+                </h2>
+                <p>
+                ${date}
+                </p>
+                <p>
+                ${content}
+                </p>
+            </div>
+           
+            <script type="text/javascript" src="/ui/main.js">
+            </script>
+        </body>
+    </html>` 
+    ;
+    return htmlTemplate;
+}
+app.get('/Technology_Article', function (req, res) {
+  res.send(createTemplate(essay));
+});
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
