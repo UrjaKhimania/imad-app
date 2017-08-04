e2c9a3bca7cdb428c9cd2f14cf0ad222e8067736
UrjaKhimania <30670182+UrjaKhimania@users.noreply.github.com>
2017-08-04 14:19:16 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,7 @@
 console.log('Loaded!');
 var up = document.getElementById("up");
 var icon = document.getElementById("icon");
-up.onclick = icon.move
+up.onclick = function(){
+    var interval = setInterval(moveRight, 100);
+    icon.style.marginLeft = "50px";
+};
