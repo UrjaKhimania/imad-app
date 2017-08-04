222b720b6337023926be8ddd078fb4291c2dea06
UrjaKhimania <30670182+UrjaKhimania@users.noreply.github.com>
2017-08-04 14:37:18 -0400

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -4,5 +4,5 @@ var icon = document.getElementById("icon");
 up.onclick = function(){
     var icon = document.getElementById("icon");
     var interval = setInterval(moveRight, 100);
-    icon.style.marginLeft = "50px";
+    icon.style.marginLeft = "100px";
 };
