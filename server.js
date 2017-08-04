6582b91139bde553d16998d241197f94e4c67816
UrjaKhimania <30670182+UrjaKhimania@users.noreply.github.com>
2017-08-03 21:14:13 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,14 @@ var essay = {
     heading: "What Really is the Impact of Technology on Teens?",
     date: `August 3rd, 2017
            <br/> --By Urja Khimania`,
-    content: "heeeeeeeeeeeeeeeeeeuuuuuuyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy",
+    content: `<p>Technology has proven to be very dangerous for the teenage population in the current society, that there is now a huge 
+        threat of a more violent, solitary, and vacuous future for us. As the addictive technology continues to grow, it lures the current 
+        generation to it, whose developing brains give them the ability to adapt to almost anything they are exposed to without letting 
+        them make any serious decisions for their fragile lives. Despite being highly beneficial for learners like teenagers, technology, 
+        for the most part, is proving to be a serious threat to their smooth lives since they are usually not aware of how to utilize it 
+        wisely without letting it influence their real world. This way, they easily get hooked up on negative aspects of technology like 
+        violent games and texting which bring tremendous impact to their personal lives. Hence, technology harms teens more than it helps 
+        them because constant use of it promotes violence and hinders their social skills that are necessary for human existence. </p>`,
 };
 
 function createTemplate (data){
