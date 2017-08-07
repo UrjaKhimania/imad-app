cc6e5ec073b0cd7476faac05224a6cd9f9a8e8fe
UrjaKhimania <30670182+UrjaKhimania@users.noreply.github.com>
2017-08-07 16:59:24 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++
@@ -1,7 +0,0 @@
-var button = document.getElementById("like");
-var counter = 0;
-button.onclick = function(){
-    counter = counter + 1;
-    var span = document.getElementById("span");
-    span.innerHTML = counter.toString();
-};
