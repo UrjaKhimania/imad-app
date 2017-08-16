e10972622ef62507f8083ca64cb0dbf5bb64f660
UrjaKhimania <30670182+UrjaKhimania@users.noreply.github.com>
2017-08-15 21:21:45 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+app.get('/login', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'login.html'));
+});
+
 app.get('/flappy', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'flappy.html'));
 });
