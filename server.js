d80021e6be38fa8df80c3648422f72ce0a4f586e
UrjaKhimania <30670182+UrjaKhimania@users.noreply.github.com>
2017-08-18 14:48:30 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-app.use(bodyParser.json());
+
 
 app.get('/login', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'login.html'));
